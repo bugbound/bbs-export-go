9d57b590e8169d7f2441dcae383c36f1e12fe857
John Doe <johndoe@example.com>
2020-06-15 18:28:38 +0100

    v1

--- bbsexport.go
+++ bbsexport.go
@@ -25,7 +25,7 @@ func main() {
         //export_domains()
         foo1 := new(PagedRecords) // or &Foo{}
         getJson("http://bbsstore-service:7002/api/dns_store?page=1", foo1)
-        println(foo1.num_results)
+        println(foo1)
     }
 }
 
