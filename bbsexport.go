39dcc984c05cb5fce67d6c52217b61758980e84c
John Doe <johndoe@example.com>
2020-06-15 18:30:27 +0100

    v1

--- bbsexport.go
+++ bbsexport.go
@@ -12,7 +12,6 @@ import (
 
 type PagedRecords struct {
     num_results int  
-    objects   []DomainRecord 
 }
 
 type DomainRecord struct {
@@ -25,7 +24,7 @@ func main() {
         //export_domains()
         foo1 := new(PagedRecords) // or &Foo{}
         getJson("http://bbsstore-service:7002/api/dns_store?page=1", foo1)
-        println(foo1)
+        println(foo1.num_results)
     }
 }
 
