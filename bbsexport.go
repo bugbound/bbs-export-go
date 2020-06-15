e4300c098d298f0ffeab25d4d0d929de629801c9
John Doe <johndoe@example.com>
2020-06-15 18:41:18 +0100

    v1

--- bbsexport.go
+++ bbsexport.go
@@ -28,7 +28,7 @@ func main() {
         getJson("http://bbsstore-service:7002/api/dns_store?page=10", foo1)
         //println(foo1.Objects[0].Domain)
         for i, s := range foo1.Objects {
-            fmt.Println(i, s.domain)
+            fmt.Println(i, s.Domain)
         }
     }
 }
